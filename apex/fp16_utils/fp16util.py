e7759ba3a0d69e9bdf2fc8cd41ea70a08054733c
Minho Shim <6764739+minostauros@users.noreply.github.com>
2021-11-09 20:42:11 +0900

    change: affine=False batchnorms are not converted

--- apex/fp16_utils/fp16util.py
+++ apex/fp16_utils/fp16util.py
@@ -62,7 +62,7 @@ def convert_network(network, dtype):
     Converts a network's parameters and buffers to dtype.
     """
     for module in network.modules():
-        if isinstance(module, torch.nn.modules.batchnorm._BatchNorm) and module.affine is True:
+        if isinstance(module, torch.nn.modules.batchnorm._BatchNorm):
             continue
         convert_module(module, dtype)
         if isinstance(module, torch.nn.RNNBase) or isinstance(module, torch.nn.modules.rnn.RNNBase):
